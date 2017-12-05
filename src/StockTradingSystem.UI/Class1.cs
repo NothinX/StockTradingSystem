d19772b0e06facf1a8cf5c7654215e8ead2cbf96
NothinX <chamjy.524@gmail.com>
2017-12-05 09:05:34 +0800

    1.创建LibCore 2.创建LibUI 3.更改Client的程序集名称

---
+++ src/StockTradingSystem.UI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTradingSystem.UI
+{
+    public class Class1
+    {
+    }
+}
