b0eebd00afebfd122c383c77257a2be6f2ad29f6
NothinX <chamjy.524@gmail.com>
2017-12-25 00:30:04 +0800

    加入了Pbkdf2算法加密密码

---
+++ src/StockTradingSystem.Core/Model/Security.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StockTradingSystem.Core.Model
+{
+    public class Security
+    {
+        public static string Pbkdf2(string passwd)
+        {
+            var salt = Encoding.Unicode.GetBytes("😂😘😍😊😁😭😜");
+            var r = new Rfc2898DeriveBytes(passwd, salt, 8192);
+            return BitConverter.ToString(r.GetBytes(127)).Replace("-", "");
+        }
+    }
+}
