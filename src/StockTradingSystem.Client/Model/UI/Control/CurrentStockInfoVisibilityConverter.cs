f6191d789ada59ee3bf34a933c63f31f767665dd
NothinX <chamjy.524@gmail.com>
2017-12-28 19:21:43 +0800

    交易页面查看当前选中股票

---
+++ src/StockTradingSystem.Client/Model/UI/Control/CurrentStockInfoVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using StockTradingSystem.Client.Model.Info;
+
+namespace StockTradingSystem.Client.Model.UI.Control
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// CurrentStockInfo和Visibility的属性转换器，用于TradeView
+    /// </summary>
+    [ValueConversion(typeof(StockInfo),typeof(Visibility), ParameterType = typeof(bool?))]
+    public class CurrentStockInfoVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var flag = (bool)(parameter ?? true);
+            if (flag) return value is StockInfo ? Visibility.Visible : Visibility.Collapsed;
+            return value is StockInfo ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
